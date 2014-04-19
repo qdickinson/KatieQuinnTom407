93ffcb789a8ed9d2b917dc34cf20b8b2d4fa5217
Katie Hildebrand <khildebrand@wisc.edu>
2014-04-19 14:12:28 -0500

    Redid mol activity layout and began activity implementation

--- LabAssistant/src/com/example/labassistant/MolActivity.java
+++ LabAssistant/src/com/example/labassistant/MolActivity.java
@@ -6,61 +6,64 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 
 public class MolActivity extends Activity {
 
-	Button   okbutton;
-	EditText compoundtext;
+    private Spinner elementsSpinner;
+    private int index;
+    String[] elements;
+
+    Double[] molWeights = {227.0, 107.87, 26.982, 39.948, 74.922, 210.0, 196.97, 10.811, 137.33, 9.0122, 272.0,
+            208.98, 247.0, 79.904, 12.011, 40.078, 112.41, 140.12, 251.0, 35.453, 247.0, 285.0, 58.933, 51.996,
+            132.91, 63.546,268.0, 281.0, 162.5, 167.26, 252.0, 151.96, 183.998, 55.845, 287.0, 257.0, 223.0, 69.723,
+            157.25, 72.64, 1.0079, 4.0026, 178.49, 200.59, 164.93, 277.0, 126.9, 114.82, 192.22, 39.098, 83.798, 138.91,
+            6.941, 262.0, 174.97, 291.0, 258.0, 24.305, 54.938, 95.96, 276.0, 14.007, 22.99, 92.906, 144.24, 20.18, 58.693,
+            259.0, 237.0, 15.999, 190.23, 30.974, 231.04, 207.2, 106.42, 145.0, 209.0, 140.91, 195.08, 244.0, 226.0, 85.468,
+            186.21, 267.0, 280.0, 102.91, 222.0, 101.07, 32.065, 121.76, 44.956, 78.96, 271.0, 28.086, 150.36, 118.71,
+            87.62, 180.95, 158.93, 98.0, 127.6, 232.04, 47.867, 204.38, 168.93, 238.03, 50.942, 183.84, 131.29,
+            88.906, 173.05, 65.38, 91.224};
 
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_mol);
 
-		    okbutton = (Button)findViewById(R.id.okbutton);
-		    compoundtext   = (EditText)findViewById(R.id.compoundtext);
+		    elementsSpinner = (Spinner) findViewById(R.id.molWeightSpinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.elements_array, android.R.layout.simple_spinner_item);
 
-		    okbutton.setOnClickListener(
-		        new View.OnClickListener()
-		        {
-		            public void onClick(View view)
-		            {
-		                Log.v("EditText", compoundtext.getText().toString());
-		            }
-		        });
-		}
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            elementsSpinner.setAdapter(adapter);
 
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.main, menu);
-			return true;
+            elementsSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                    index = arg0.getSelectedItemPosition();
+                    elements = getResources().getStringArray(R.array.elements_array);
+                }
+
+                public void onNothingSelected(AdapterView<?> arg0) {
+                    //do nothing
+                }
+            });
 		}
 		
 		public void onButtonClick(View v)
 		{
 			switch(v.getId())
 			{
-			
-			case R.id.okbutton:
-				String text = compoundtext.getText().toString();
-				EditText finalmassText = (EditText)findViewById(R.id.finalmasstext);
-				finalmassText.setText(text);
+                case R.id.molAddElementButton:
+                    break;
+
+                case R.id.molCalculatebutton:
+                    break;
 				
-				break;
-				
-			case R.id.clearbutton:
-				
-				
-				
-				break;
-			
-			
-			
-			default:
+			    case R.id.clearbutton:
+			        break;
+
+                default:
 			}
 			
 		}
