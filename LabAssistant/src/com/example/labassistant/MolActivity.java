df8924eeb4fa25447ab2504ed5c908763980d554
Katie Hildebrand <khildebrand@wisc.edu>
2014-04-19 15:16:00 -0500

    Calculates mol mass after compound is input

--- LabAssistant/src/com/example/labassistant/MolActivity.java
+++ LabAssistant/src/com/example/labassistant/MolActivity.java
@@ -9,16 +9,24 @@ import android.view.View;
 import android.widget.*;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import java.util.ArrayList;
+
 
 public class MolActivity extends Activity {
 
     private Spinner elementsSpinner;
     private int index;
-    String[] elements;
+    private String[] elements;
+    private ArrayList<Integer> indices = new ArrayList<Integer>();
+    private ArrayList<Integer> subscripts = new ArrayList<Integer>();
+    private String compound = "";
+    private TextView molCompoundView;
+    private TextView molDisplayValue;
 
-    Double[] molWeights = {227.0, 107.87, 26.982, 39.948, 74.922, 210.0, 196.97, 10.811, 137.33, 9.0122, 272.0,
+
+    private Double[] molWeights = {227.0, 107.87, 26.982, 39.948, 74.922, 210.0, 196.97, 10.811, 137.33, 9.0122, 272.0,
             208.98, 247.0, 79.904, 12.011, 40.078, 112.41, 140.12, 251.0, 35.453, 247.0, 285.0, 58.933, 51.996,
-            132.91, 63.546,268.0, 281.0, 162.5, 167.26, 252.0, 151.96, 183.998, 55.845, 287.0, 257.0, 223.0, 69.723,
+            132.91, 63.546, 268.0, 281.0, 162.5, 167.26, 252.0, 151.96, 183.998, 55.845, 287.0, 257.0, 223.0, 69.723,
             157.25, 72.64, 1.0079, 4.0026, 178.49, 200.59, 164.93, 277.0, 126.9, 114.82, 192.22, 39.098, 83.798, 138.91,
             6.941, 262.0, 174.97, 291.0, 258.0, 24.305, 54.938, 95.96, 276.0, 14.007, 22.99, 92.906, 144.24, 20.18, 58.693,
             259.0, 237.0, 15.999, 190.23, 30.974, 231.04, 207.2, 106.42, 145.0, 209.0, 140.91, 195.08, 244.0, 226.0, 85.468,
@@ -30,6 +38,9 @@ public class MolActivity extends Activity {
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_mol);
+
+            molCompoundView = (TextView) findViewById(R.id.molCompoundView);
+            molDisplayValue = (TextView) findViewById(R.id.molDisplayValue);
 
 		    elementsSpinner = (Spinner) findViewById(R.id.molWeightSpinner);
             ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -55,12 +66,42 @@ public class MolActivity extends Activity {
 			switch(v.getId())
 			{
                 case R.id.molAddElementButton:
+                    EditText subscriptText = (EditText) findViewById(R.id.molEditSubscript);
+                    String subscript = subscriptText.getText().toString();
+                    if (subscript.equals("")) {
+                        subscripts.add(1);
+                    }
+                    else {
+                        subscripts.add(Integer.parseInt(subscript));
+                    }
+                    indices.add(index);
+                    compound = compound + elements[index] + subscript;
+                    molCompoundView.setText(compound);
+                    subscriptText.setText("");
                     break;
 
                 case R.id.molCalculatebutton:
+                    double mass = 0;
+                    for (int i = 0; i < subscripts.size(); i++) {
+                        double value = subscripts.get(i) * molWeights[indices.get(i)];
+                        mass += value;
+                    }
+
+                    String molarMass = Double.toString(mass);
+                    molDisplayValue.setText(molarMass);
+                    indices.clear();
+                    subscripts.clear();
+                    compound = "";
+                    molCompoundView.setText("");
+
                     break;
 				
 			    case R.id.clearbutton:
+                    indices.clear();
+                    subscripts.clear();
+                    compound = "";
+                    molCompoundView.setText("");
+                    molDisplayValue.setText("");
 			        break;
 
                 default:
