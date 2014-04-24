44c0ea2979762dbbec2e4c7875b68fac9d4a08c2
Katie Hildebrand <khildebrand@wisc.edu>
2014-04-23 21:10:38 -0500

    Layout for calculating solution's molarity, changed input type to numberDecimal

--- LabAssistant/src/com/example/labassistant/SolutionsActivity.java
+++ LabAssistant/src/com/example/labassistant/SolutionsActivity.java
@@ -4,22 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
 public class SolutionsActivity extends Activity {
+
+    private Spinner elementsSpinner;
+    private int index;
+    private String[] elements;
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.solutions_type_select);
 		}
 		
-		public void onRadioButtonClicked(View v)
+		public void onButtonClick(View v)
 		{
 			switch(v.getId())
 			{
                 case R.id.molarityRadioButton:
-                    Toast.makeText(getApplicationContext(),"Molarity Option to be Implemented Later", Toast.LENGTH_LONG ).show();
+                    setContentView(R.layout.activity_solutions);
+                    elementsSpinner = (Spinner) findViewById(R.id.solutionSpinner);
+                    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                            R.array.elements_array, android.R.layout.simple_spinner_item);
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    elementsSpinner.setAdapter(adapter);
+
+                    elementsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                            index = arg0.getSelectedItemPosition();
+                            elements = getResources().getStringArray(R.array.elements_array);
+                        }
+
+                        public void onNothingSelected(AdapterView<?> arg0) {
+                            //do nothing
+                        }
+                    });
+                    //Toast.makeText(getApplicationContext(),"Molarity Option to be Implemented Later", Toast.LENGTH_LONG ).show();
                     break;
                 case R.id.ppmRadioButton:
                     Toast.makeText(getApplicationContext(),"ppm Option to be Implemented Later", Toast.LENGTH_LONG ).show();
@@ -30,11 +55,27 @@ public class SolutionsActivity extends Activity {
                 case R.id.percentRadioButton:
                     Toast.makeText(getApplicationContext(),"% [] Option to be Implemented Later", Toast.LENGTH_LONG ).show();
                     break;
+                case R.id.solAddCompound:
+                    break;
+                case R.id.solClearCompound:
+                    break;
+                case R.id.soluteMolUnits:
+                    break;
+                case R.id.soluteGramUnits:
+                    break;
+                case R.id.solveMassRadioButton:
+                    break;
+                case R.id.solveMolarityRadioButton:
+                    break;
+                case R.id.solCalcButton:
+                    break;
 			default:
                 break;
 			}
 			
 		}
+
+    }
 	
-	}
+
 
